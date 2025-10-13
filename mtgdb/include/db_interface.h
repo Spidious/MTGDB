263a0c4398414b3b8039daf6d59fbc0a4750da19
Luke Deffenbaugh <lukedeffenbaugh@gmail.com>
2025-10-13 15:26:29 -0500

    Add other interface classes for connecting to maria and mysql databases

--- mtgdb/include/db_interface.h
+++ mtgdb/include/db_interface.h
@@ -7,15 +7,44 @@
 #include <iostream>
 #include <sqlite3.h>
 
+/// <summary>
+/// Establishes a connection to a database using the specified URI.
+/// </summary>
+class DatabaseConnector {
+	public:
+		DatabaseConnector() = default;
+		/// <summary>
+		/// Establishes a connection to the specified URI.
+		/// </summary>
+		/// <param name="uri">A null-terminated string containing the URI to connect to.</param>
+		/// <returns>True if the connection was successful; otherwise, false.</returns>
+		virtual bool Connect(const char* uri) = 0;
+		/// <summary>
+		/// Executes the specified SQL statement.
+		/// </summary>
+		/// <param name="sql">A pointer to a null-terminated string containing the SQL statement to execute.</param>
+		/// <returns>True if the SQL statement was executed successfully; otherwise, false.</returns>
+		virtual bool Execute(const char* sql) = 0;
+		/// <summary>
+		/// Closes the resource or connection associated with the object.
+		/// </summary>
+		virtual void Close() = 0;
+		/// <summary>
+		/// Destroys the DatabaseConnector object and releases any associated resources.
+		/// </summary>
+		virtual ~DatabaseConnector() = 0;
+};
+
+/// <summary>
+/// Provides an interface for managing database connections and executing SQL commands.
+/// </summary>
 class DBInterface {
-	sqlite3* db;
-	char* errMsg;
-	int rc;
+	DatabaseConnector* connector;
 
 	public:
 		/// <summary>
 		/// Initialize the database connection 
-		/// TODO: Handle SQLite or MySQL/MariaDB connections
+		/// TODO: Handle SQLite or MySQ	    L/MariaDB connections
 		/// TODO: Add connection parameters (host, user, password, dbname)
 		/// </summary>
 		DBInterface();
@@ -35,6 +64,42 @@ class DBInterface {
 		///	Call close on destruction
 		/// </summary>
 		~DBInterface();
+
+	private:
+		/// <summary>
+		/// Database connector for SQLite
+		/// </summary>
+		class sqlite_connector : public DatabaseConnector {
+			sqlite3* db;
+			char* errMsg;
+			int rc;
+
+			public:
+				sqlite_connector();
+				bool Connect(const char* uri) override;
+				bool Execute(const char* sql) override;
+				void Close() override;
+		};
+		/// <summary>
+		/// Database Connector for MySQL
+		/// </summary>
+		class mysql_connector : public DatabaseConnector {
+			public:
+				mysql_connector() = default;
+				bool Connect(const char* uri) override;
+				bool Execute(const char* sql) override;
+				void Close() override;
+		};
+		/// <summary>
+		/// Database Connector for MariaDB
+		/// </summary>
+		class mariadb_connector : public DatabaseConnector {
+			public:
+				mariadb_connector() = default;
+				bool Connect(const char* uri) override;
+				bool Execute(const char* sql) override;
+				void Close() override;
+		};
 };
 
 #endif 
--- mtgdb/lib/db_interface.cpp
+++ mtgdb/lib/db_interface.cpp
@@ -1,43 +1,115 @@
 #include "db_interface.h"
-#include <sqlite3.h>
 
 using namespace std;
 
+void DatabaseConnector::Close() {}
+
+DatabaseConnector::
+~DatabaseConnector() {
+	Close();
+	cout << "DatabaseConnector destroyed" << endl;
+}
+
+/*
+#################################################
+# SQLite Connector Implementation
+#################################################
+*/
+
+DBInterface::sqlite_connector::
+sqlite_connector() {
+	db = nullptr;
+	errMsg = nullptr;
+	int rc = 0;
+}
+
+bool DBInterface::sqlite_connector::
+Connect(const char* uri) {
+	rc = sqlite3_open(uri, &db);
+	return(rc == SQLITE_OK);
+}
+
+bool DBInterface::sqlite_connector::
+Execute(const char* sql) {
+	rc = sqlite3_exec(db, sql, nullptr, nullptr, &errMsg);
+	return(rc == SQLITE_OK);
+}
+
+void DBInterface::sqlite_connector::
+Close() {
+	sqlite3_close(db);
+}
+
+/*
+#################################################
+# MySQL Connector Implementation
+#################################################
+*/
+
+bool DBInterface::mysql_connector::
+Connect(const char* uri) {
+	return true;
+}
+
+bool DBInterface::mysql_connector::
+Execute(const char* sql) {
+	return true;
+}
+
+void DBInterface::mysql_connector::
+Close() {
+}
+
+/*
+#################################################
+# MariaDB Connector Implementation
+#################################################
+*/
+
+bool DBInterface::mariadb_connector::
+Connect(const char* uri) {
+	return true;
+}
+
+bool DBInterface::mariadb_connector::
+Execute(const char* sql) {
+	return true;
+}
+
+void DBInterface::mariadb_connector::
+Close() {
+}
+
+/*
+#################################################
+# DBInterface Implementation
+#################################################
+*/
+
+
 // Default constructor to initialize database connection
 DBInterface::DBInterface() {
-	errMsg = nullptr;
-	rc = sqlite3_open("test.db", &db);
-	// Initialize database connection
-	if (rc) {
-		cerr << "Can't open database: " << sqlite3_errmsg(db) << endl;
-	}
-	else {
-		cout << "Opened database successfully" << endl;
+	connector = new sqlite_connector();
+	if(!connector->Connect("test.db")) {
+		cerr << "Can't open database" << endl;
 	}
 }
 
 // Execute SQL command on database
 void DBInterface::Execute(const char* sql) {
-	// Execute SQL command
-	rc = sqlite3_exec(db, sql, nullptr, nullptr, &errMsg);
-	if (rc != SQLITE_OK) {
-		cerr << "SQL error: " << errMsg << endl;
-		sqlite3_free(errMsg);
-	}
-	else {
-		cout << "SQL executed successfully" << endl;
+	if (!connector->Execute(sql)) {
+		cerr << "SQL error" << endl;
 	}
 }
 
 // Close database connection
 void DBInterface::Close() {
-	// Close database connection
-	sqlite3_close(db);
+	connector->Close();
 }
 
 // Destructor to close database connection
 DBInterface::~DBInterface() {
 	// Close database connection
 	Close();
-	cout << "Database connection closed" << endl;
+	cout << "Database connection closed!!!" << endl;
 }
