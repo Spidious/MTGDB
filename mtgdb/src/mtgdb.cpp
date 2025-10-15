0d17e4838887e9c31714981c09283249b179ef0e
spidious <lukedeffenbaugh@gmail.com>
2025-10-14 23:21:06 -0500

    Clean main file

--- mtgdb/src/mtgdb.cpp
+++ mtgdb/src/mtgdb.cpp
@@ -7,14 +7,20 @@ using namespace std;
 
 int main()
 {
-	// TODO: DB Type should be configurable by user.
 	// Open Database
 	DBInterface db(DB_SQLITE, "test.db");
+
+	// Create api
 	ScryfallAPI api;
+
+	// search for card based on ID
 	cout << "Random card by ID:" << endl;
 	api.BasicSearch("5b94f37f-ebdf-4b79-a615-58331d27cf4e");
 	cout << endl;
+
+	// Search for card based on information
 	cout << "Searching random card by name:" << endl;
 	api.BasicSearch("Mechanical Mobster", "Artifact", "", 168);
+
 	return 0;
 }
