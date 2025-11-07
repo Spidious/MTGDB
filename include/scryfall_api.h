2762937cdf678b4d43257a33a85d6016863f0a8d
Luke Deffenbaugh <lukedeffenbaugh@gmail.com>
2025-11-07 15:52:51 -0600

    Reformatted doc comments
    Scryfall API header doc comments closer to doxygen

--- include/scryfall_api.h
+++ include/scryfall_api.h
@@ -26,10 +26,20 @@ using namespace nlohmann;
 
 namespace Scryfall
 {
+	/********************************************************************************************************
+	* Global Namespace Variables																			*
+	********************************************************************************************************/
+
+	/**
+	 * Set of all supported Scryfall object types
+	 */
 	const unordered_set<string> ObjectTypes = {
 		"empty", "error", "list", "set", "card", "ruling", "card_symbol", "catalog", "bulk_data"
 	};
 
+	/**
+	 * Set of fields used in Scryfall Searches
+	 */
 	const unordered_set<string> SearchFields = {
 		"n", "name",
 		"c", "color",
@@ -52,252 +62,390 @@ namespace Scryfall
 		"new",
 	};
 
+	/**
+	 * Set of operators used in Scryfall searches
+	 */
 	const unordered_set<string> SearchOps = {
 		":", ">", "<", ">=", "<=", "!=", "-"
 	};
 
+	/**
+	 * Search item used in Scrfall API searches
+	 */
 	struct SearchItem {
 		string field;
 		string op;
 		string value;
 	} typedef searchitem;
 
-	// ###################################################################
+	/********************************************************************************************************
+	* Helper Functions																						*
+	********************************************************************************************************/
 
-	///	<summary>
-	///	Base object to hold JSON information returned by the Scryfall API
-	///	</summary>
+	/**
+	 * Callback function for writing received data, typically used with data transfer libraries such as libcurl.
+	 * @param ptr Pointer to the received data buffer.
+	 * @param size Size of each data element in bytes.
+	 * @param nmemb Number of data elements to write.
+	 * @param userdata Pointer to user-defined data or context.
+	 * @return The number fo bytes actually handled. Returning a value different from size * nmemb indicates error
+	 */
+	static size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata);
+
+	/********************************************************************************************************
+	* Scryfall Object and derived classes																	*
+	********************************************************************************************************/
+
+	/**
+	 * Polymorphic Object containing the JSON information returned by Scryfall API
+	 */
 	class ScryfallObject : protected json
 	{
-		///<summary>Base information object type</summary>
+		/**
+		 * String object type (To determine ScryfallObject derived class)
+		 */
 		const std::string obj_type;
 
 	protected:
-		///<summary>
-		///Generate a json Error
-		///</summary>
-		///<param name="err">String description of the error</param>
-		///<param name="status">custom integer status (Default: -1)</param>
-		///<returns>Unique ScryfallObject pointer (may be of a derived class)</returns>
+		/**
+		 * Generate ScryError json based object
+		 * @param err String description of error
+		 * @param status Integer status (Default: -1)
+		 * @return Unique ScryfallObject pointer to ScryError
+		 */
 		static std::unique_ptr<ScryfallObject> generate_error(const std::string err, const int status = -1);
 
 	public:
-		///<summary>
-		///Constructor for ScryfallObject
-		///</summary>
-		///<param name="j">Json object</param>
+		/**
+		 * Constructor to build Scryfall Object
+		 * @param j Json object
+		 */
 		explicit ScryfallObject(const json& j);
 
-		///<summary>
-		///Factory function to determine appropriate derived class
-		///Overloaded for JSON string
-		///</summary>
-		///<param name="json">JSON represented as string object</param>
-		///<returns>Unique pointer to ScryfallObject</returns>
-		///<remarks>Returns a ScryError upon fail</remarks>
+		/**
+		 * Factory function -> Determine proper derived class given JSON string
+		 * @param json String JSON Scryfall object
+		 * @return Unique Scryfall Object Pointer to Scryfall Object or derived class
+		 * @exception ScryError Object ScryError unique pointer is returned upon fail
+		 */
 		static std::unique_ptr<ScryfallObject> from_json(const string& json);
 
+		/**
+		 *	Factory function -> Determine proper derived class given JSON string
+		 * @param json JSON Scryfall object
+		 * @return Unique Scryfall Object Pointer to Scryfall Object or derived class
+		 * @exception ScryError Object ScryError unique pointer is returned upon fail
+		 */
 		static std::unique_ptr<ScryfallObject> from_json(const json& json);
+
+		/**
+		 * Retrieve the object type value
+		 * @return string object type
+		 */
 		std::string get_object_type() const;
+
+		/**
+		 * Retrieve the name value of object
+		 * @return Name of object or empty string if one is unavailable
+		 */
 		virtual std::string get_name() const;
+
+		/**
+		 * Retrieve attribute given the Key (arg)
+		 * @tparam T Expected type of returned json argument
+		 * @param arg Key
+		 * @param default_value Default value if one is not found (Default T{})
+		 * @return Value of Json object at key "arg" of type "T"
+		 * @todo Handle errors for edge-cases
+		 */
 		template <typename T> T get_attr(const std::string& arg, const T& default_value = T{}) const {
 			return this->value(arg, default_value);
 		}
+
+		/**
+		 * Default destructor
+		 */
 		virtual ~ScryfallObject() = default;
 	};
 
-	// Specific Scryfall Error object
+	/**
+	 * ScryfallObject derived class for Error objects
+	 * @todo potentially make this inherit std::exception
+	 */
 	class ScryError final: public ScryfallObject
 	{
+		/**
+		 * Message or description of the error
+		 */
 		std::string err_msg;
+
 	public:
+		/**
+		 * Basic constructor
+		 * @param j JSON object
+		 */
 		explicit ScryError(const json& j);
+
+		/**
+		 * Typical
+		 * @return Return the err_msg
+		 */
 		std::string what();
+
+		/**
+		 * Default destructor
+		 */
 		~ScryError() override = default;
 	};
 
-	// Specific Scryfall List object
+	/**
+	 * ScryfallObject derived class for List objects
+	 */
 	class ScryList final: public ScryfallObject
 	{
+		/**
+		 * Direct API url for continuation of list
+		 */
 		std::string next_url;
+		/**
+		 * vector array of objects contained in List
+		 */
 		std::vector<std::shared_ptr<ScryfallObject>> data;
 
+		/**
+		 * Non-Polymorphic call function to retrieve the next_url
+		 * @return another list object unique pointer
+		 */
 		std::unique_ptr<ScryfallObject> call_next_list() const;
 
 	public:
+		/**
+		 * Basic Constructor
+		 * @param j JSON object
+		 */
 		explicit ScryList(const json& j);
+
+		/**
+		 * Retrieves the length of the associated list of data
+		 * @return integer size of data vector
+		 */
 		int size() const;
+
+		/**
+		 * Retrieves the shared pointer to the object at vector index
+		 * @param index integer index
+		 * @return shared pointer to object at index "index"
+		 * @todo disallow indexes greater than or equal to size()
+		 */
 		std::shared_ptr<ScryfallObject> at(int index) const;
+
+		/**
+		 * Default destructor
+		 */
 		~ScryList() override = default;
 	};
 
-	// Specific Scryfall Set object
+	/**
+	 * ScryfallObject derived class for Set objects
+	 */
 	class ScrySet final: public ScryfallObject
 	{
 	public:
+		/**
+		 * Basic Constructor
+		 * @param j JSON object
+		 */
 		explicit ScrySet(const json& j) : ScryfallObject(j) {}
+
+		/**
+		 * Default destructor
+		 */
 		~ScrySet() override = default;
 	};
 
-	// Specific Scryfall Card object
+	/**
+	 * ScryfallObject derived class for Card objects
+	 */
 	class ScryCard final: public ScryfallObject
 	{
 	public:
+		/**
+		 * Basic Constructor
+		 * @param j JSON object
+		 */
 		explicit ScryCard(const json& j) : ScryfallObject(j) {}
+
+		/**
+		 * Return the name of the Card
+		 * @return String card name
+		 */
 		std::string get_name() const override;
+
+		/**
+		 * Default destructor
+		 */
 		~ScryCard() override = default;
 	};
 
-	// Specific Scryfall Ruling object
+	/**
+	 * ScryfallObject derived class for Card objects
+	 */
 	class ScryRuling final: public ScryfallObject
 	{
 	public:
+		/**
+		 * Basic Constructor
+		 * @param j JSON object
+		 */
 		explicit ScryRuling(const json& j) : ScryfallObject(j) {}
+
+		/**
+		 * Default destructor
+		 */
 		~ScryRuling() override = default;
 	};
 
-	// ###################################################################
+	/********************************************************************************************************
+	* Scryfall API Wrapper																					*
+	********************************************************************************************************/
 
-	/// <summary>
-	/// Callback function for writing received data, typically used with data transfer libraries such as libcurl.
-	/// </summary>
-	/// <param name="ptr">Pointer to the received data buffer.</param>
-	/// <param name="size">Size of each data element in bytes.</param>
-	/// <param name="nmemb">Number of data elements to write.</param>
-	/// <param name="userdata">Pointer to user-defined data or context.</param>
-	/// <returns>The number of bytes actually handled. Returning a value different from (size * nmemb) will signal an error to the caller.</returns>
-	static size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata);
-
-	/// <summary>
-	/// Api interface for api.scryfall.com
-	/// </summary>
+	/**
+	 * Scryfall API wrapper
+	 */
 	class ScryfallAPI {
 
-		/// <summary>
-		/// Mutex to ensure thread safety and rate limiting
-		/// </summary>
+		/**
+		 * Mutex to ensure thread safety and rate limiting
+		 */
 		static mutex api_mutex;
 
-		/// <summary>
-		/// A pointer to http headers
-		/// </summary>
+		/**
+		 * A pointer to http headers
+		 */
 		struct curl_slist* headers;
 
-		/// <summary>
-		/// A pointer to the curl client
-		/// </summary>
+		/**
+		 * A pointer to the CURL client
+		 */
 		CURL* cli;
 
-		/// <summary>
-		/// Static function to encode strings with % encoding for URLs
-		/// </summary>
-		/// <param name="str_value">String value to be encoded</param>
-		/// <returns>Encoded string</returns>
+		/**
+		 * Encode strings with % encoding for URLs
+		 * @param str_value String value to be encoded
+		 */
 		static string url_encode(const string& str_value);
 
-		/// <summary>
-		/// Validate the search query.
-		/// Query should be ready for URL (Decoded): c:rg+mana:{G}+t:bird
-		/// </summary>
-		/// <param name="str_value"></param>
-		/// <returns></returns>
+		/**
+		 * Validate the search query.
+		 * @param query string query ready for URL (Decoded): c:rg+mana:{G}+t:bird
+		 * @return boolean value denoting whether validation passed/failed
+		 */
 		static bool validate_search(const string& query);
 
-		/// <summary>
-		/// Calls an api endpoint, enforcing rate limit
-		/// </summary>
-		/// <param name="path">The specific endpoint needing to be called</param>
-		/// <param name="buffer">Buffer in which to put the
-		/// <returns>Result of the API call</returns>
+		/**
+		 * CURL call to the API
+		 * @param path string https path to API
+		 * @param buffer buffer in which to write the string
+		 * @return CURLcode status
+		 */
 		CURLcode call_api(const string& path, const void* buffer);
 
+		/**
+		 * mark ScryLists as friend so that they may access private members
+		 * @todo: Instead of this, create some kind of public raw_call for the API
+		 */
 		friend ScryList;
 
 	public:
 
-		/// <summary>
-		/// recursive parse a scryfall API query into base format (Non-encoded)
-		/// </summary>
-		/// <param name="queries">list of {field, operator, value} ex. => {{"name", ":", "fish"}, {"mana", ">=", "ug"}}</param>
-		/// <returns>fully parsed array of search queries. ex. => name:fish+man>=ug</returns>
+		/**
+		 * Recursive parse a Scryfall API query into base format
+		 * @param queries List of {field, operator, value} ex. => {{"name", ":", "fish"}, {"mana", ">=", "ug"}}
+		 * @return Fully parsed array of search quereis. ex. => name:fish+man>=ug
+		 */
 		static string parse_query(vector<searchitem> queries);
 
-		/// <summary>
-		/// An API constructor that initializes the curl client and sets required headers
-		/// </summary>
+		/**
+		 * Basic Constructor
+		 */
 		ScryfallAPI();
 
-		/// <summary>
-		/// Search for a card by its unique SCRYFALL ID
-		/// Can also be used with any of the special keywords: random, random-rare, random-foil, random-art
-		/// </summary>
-		/// <param name="id">Scryfall ID or Keyword</param>
-		/// <returns>Raw API Result</returns>
+		/**
+		 * Basic API search based on string arg
+		 * @param id basic search keywords: <Scryfall_ID>, random, random-rare, random-foil, random-art
+		 * @return Unique pointer to ScryfallObject or derived class
+		 */
 		std::unique_ptr<ScryfallObject> BasicSearch(const string& id);
 
-		/// <summary>
-		/// Performs a basic search using the specified name, type, and optional set and collection number.
-		/// </summary>
-		/// <param name="name">The name to search for.</param>
-		/// <param name="type">The type to search for.</param>
-		/// <param name="set">The set to search within (optional).</param>
-		/// <param name="collect_num">The collection number to search for (optional).</param>
-		std::unique_ptr<ScryfallObject> BasicSearch(const string& name, const string& type, const string& set = "", int collect_num = 0);
-
-		/// <summary>
-		/// Performs a scryfall search using the advanced query syntax on https://scryfall.com/docs/syntax
-		/// </summary>
-		/// <param name="query">String search query</param>
-		/// <returns>Raw API Result</returns>
+		/**
+		 * Scryfall
+		 * @param query
+		 * @return
+		 */
 		std::unique_ptr<ScryfallObject> AdvancedSearch(const string query);
 
 
-
-		/// <summary>
-		/// Destroys the ScryfallAPI object and releases any associated resources.
-		/// </summary>
+		/**
+		 * Default destructor
+		 */
 		~ScryfallAPI();
 	};
 
 	/********************************************************************************************************
-	* Scryfall API Exceptions
+	* Scryfall API Exceptions																				*
 	********************************************************************************************************/
 
-	/// <summary>
-	/// General base exception
-	/// </summary>
-	class Scryfall_Exception : public exception {
-	private:
-		string message;
+		/**
+		 * Default exception for Scryfall objects
+		 */
+		class Scryfall_Exception : public exception {
+		private:
+			/**
+			 * error message for objects
+			 */
+			string message;
 
-	public:
-		Scryfall_Exception(const string& msg = "Unknown Exception") : message(msg) {}
+		public:
+			/**
+			 * Exception Constructor
+			 * @param msg error message for exception
+			 */
+			Scryfall_Exception(const string& msg = "Unknown Exception") : message(msg) {}
 
-		/// <summary>
-		/// Returns a char* string representing the error being thrown.
-		/// </summary>
-		/// <returns>char array</returns>
-		const char* what() const noexcept override {
-			return message.c_str();
-		}
-	};
+			/**
+			 * Output exception message
+			 * @return char* msg
+			 */
+			const char* what() const noexcept override {
+				return message.c_str();
+			}
+		};
 
-	/// <summary>
-	/// Thrown when search query is incorrect or invalid
-	/// </summary>
-	class Invalid_Search_Query : public Scryfall_Exception {
-	public:
-		Invalid_Search_Query(const string msg) : Scryfall_Exception("Exception - Invalid Search Query : " + msg) {}
-	};
+		/**
+		 * Derived exception for Invalid searches
+		 */
+		class Invalid_Search_Query : public Scryfall_Exception {
+		public:
+			/**
+			 * Exception Constructor
+			 * @param msg Error message for exception
+			 */
+			Invalid_Search_Query(const string msg) : Scryfall_Exception("Exception - Invalid Search Query : " + msg) {}
+		};
 
-	/// <summary>
-	/// Thrown when API returns invalid response
-	/// </summary>
-	class Invalid_Response : public Scryfall_Exception {
-	public:
-		Invalid_Response(const string msg) : Scryfall_Exception("Exception - Invalid API Response : " + msg) {}
-	};
-}
+		/**
+		 * Derived exception for Invalid responses
+		 */
+		class Invalid_Response : public Scryfall_Exception {
+		public:
+			/**
+			 * Exception Constructor
+			 * @param msg Error message for exception
+			 */
+			Invalid_Response(const string msg) : Scryfall_Exception("Exception - Invalid API Response : " + msg) {}
+		};
+	}
+
 
 
 #endif
